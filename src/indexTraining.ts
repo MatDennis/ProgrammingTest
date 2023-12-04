4d11b3b5cc902b1df401e547cd08ab32160f3c6c
Crappermine <matthew.dennis500@gmail.com>
2023-12-04 21:24:36 +0000

    Added punctuation module

--- src/indexTraining.ts
+++ src/indexTraining.ts
@@ -245,40 +245,32 @@
 
 //STAGE 11, MAKE FUNCTIONS
 
-//import the use of the exit function.
-import { exit } from "process";
-import { kStringMaxLength } from "buffer";
-import { getHashes, hkdf } from "crypto";
+//import the use of the exit function.3
+
+//import { kStringMaxLength } from "buffer";
+//import { getHashes, hkdf } from "crypto";
 import * as fs from "fs";
 import printOut from "./printOut";
+import punctuation from "./punctuation";
 
 //obtain an input from the user to say where they want the file read from
 const argument = process.argv[2];
 //create a const to hold the information obtained from the directory of what the user has chosen. 
 const theString = fs.readFileSync(argument, 'utf-8');
-//conver the string to all lowercase letters
-const newString = theString.toLowerCase();
 
-//variable to select all punctuation and white space from the use of 'RegExp'
-const punctuation = /\w+/g;
 
-//variable to remove all whitespace and punctuation
-const matchs = newString.match(punctuation);
+const newString = punctuation(theString);
+//console.log(newString);
+
 
 //creating an object to store a string key with a number value
 const obj: Record<string,number> = { };
 
-//re write this to do something different than just exit, but it works
-//if matchs is null, do not run the code.
-if(matchs == null) {
-    exit();
-} 
-
 //counting how many words are duplicated by checking; 
 //if the entry of obj is not defined, it will count it and add the value of 1
 //else if it is defined then it will increment by 1
-for(let i = 0; i<matchs.length; i++) {
-    const holderWord = matchs[i];
+for(let i = 0; i<newString.length; i++) {
+    const holderWord = newString[i];
     if(typeof obj[holderWord] === "undefined") {3
         obj[holderWord] = 1;
     } else {
